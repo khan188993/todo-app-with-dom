b4e2651eebc80c45a99d75cfcf76b9ae78a34fdc
khan188993 <khan188993@gmail.com>
2022-09-22 06:28:25 +0600

    dom todo applicationwith json server api starter

---
+++ app.js
@@ -0,0 +1,8 @@
+const addTodoElem = document.getElementById('add-todo-form');
+const completedAllBtnElem = document.getElementById('completed-all-btn');
+const clearAllBtnElem = document.getElementById('clear-all-btn');
+const taskLeftElem = document.getElementById('task-left');
+const allFilterBtnElem = document.getElementById('all-filter-btn');
+const completedFilterBtnElem = document.getElementById('complete-filter-btn')
+const incompleteFilterBtnElem = document.getElementById('incomplete-filter-btn');
+
